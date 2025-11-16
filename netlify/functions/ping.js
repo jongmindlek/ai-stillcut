a400a905335d3d4e980aefff43ff4019e81b7b3d
jongmindlek <jongmin2323@naver.com>
2025-11-16 18:31:59 +0900

    Delete netlify/functions directory

--- netlify/functions/ping.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8",
-    },
-    body: "pong from Netlify function",
-  };
-};
